049688f3d9c8904fecef8e4fab9ba01dfea8c2f5
Henri Buyse <henri.buyse@eolane.com>
2017-05-24 17:30:30 +0200

    base code

---
+++ main.py
@@ -0,0 +1,385 @@
+#! /usr/bin/env python
+# code=utf-8
+
+import json
+
+from bottle import route, run
+from bottle import get, post
+
+from tournament import parse_tournaments
+
+LIST_TOURNAMENTS = [
+    {
+        "events": [
+            {
+                "day": "25",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495663200.0,
+                "formats": [
+                    "3x3 Féminin"
+                ],
+                "level": [
+                    "Départemental",
+                    "Régional",
+                    "National",
+                    "Pro"
+                ]
+            },
+            {
+                "day": "25",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495663200.0,
+                "formats": [
+                    "3x3 Masculin"
+                ],
+                "level": [
+                    "Départemental",
+                    "Régional",
+                    "National",
+                    "Pro"
+                ]
+            },
+            {
+                "day": "25",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495663200.0,
+                "formats": [
+                    "4x4 Mixte"
+                ],
+                "level": [
+                    "Loisir"
+                ]
+            }
+        ],
+        "href": "http://www.accro-des-tournois.com/fichetournoi.html/2312/neuvilleauxbois#amobile",
+        "ground": "grass",
+        "new": False,
+        "full": False,
+        "night": False,
+        "city": "Neuville-aux-Bois",
+        "department": "45",
+        "id": 2312,
+        "name": "Tournoi Albert Hay",
+        "address": "10 rue de Ruau, 45170 Neuville-aux-Bois, France",
+        "user": "Neuville Sports Volley-Ball",
+        "phone": "02.38.75.51.21",
+        "mail": "http://www.accro-des-tournois.com/contactorga.html/2312/neuvilleauxbois",
+        "hour": "10h",
+        "terrains": 20,
+        "gymnasium": 1,
+        "inscription": "Prévente : 8€ par joueur\nSur place : 8€ par joueur\n\nA l'ordre de Neuville Sports Volley-Ball\nA envoyer au 13 rue de Mondame, 45170 Neuville-aux-bois",
+        "additional": "Neuville Sports Volley-Ball propose à son habitude son fameux tournoi sur herbe le jeudi de l'Ascension. \nPlusieurs formules disponibles :\n- 3x3 Féminins (8€ par joueur)\n- 3x3 Masculins (8€ par joueur)\n- 4x4 Loisirs Mixte (8€ par joueur)\n- 2x2 M11 - M13 Mixte (5€ par joueur)\n- 1x1 M9 Mixte (3€ par joueur)\nInscription directement sur place de 9h à 9h30.\nRepli en salles en cas de pluie.\nSur place vous trouverez également : \n- Buffet permanent\n- Grillades\n- Frites\n- Buvette \nPossibilité de régler en carte bleue",
+        "publisher": "NSVB45"
+    },
+    {
+        "events": [
+            {
+                "day": "25",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495663200.0,
+                "formats": [
+                    "4x4 Mixte"
+                ],
+                "level": [
+                    "Loisir",
+                    "Départemental",
+                    "Régional",
+                    "National"
+                ]
+            },
+            {
+                "day": "25",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495663200.0,
+                "formats": [
+                    "3x3 Féminin"
+                ],
+                "level": [
+                    "Loisir",
+                    "Départemental",
+                    "Régional"
+                ]
+            }
+        ],
+        "href": "http://www.accro-des-tournois.com/fichetournoi.html/2367/rethel#amobile",
+        "ground": "indoor",
+        "new": False,
+        "full": False,
+        "night": False,
+        "city": "Rethel",
+        "department": "08",
+        "id": 2367,
+        "address": ", 08300 Rethel, France",
+        "mail": "http://www.accro-des-tournois.com/contactorga.html/2367/rethel",
+        "publisher": "Rik"
+    },
+    {
+        "events": [
+            {
+                "day": "25",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495663200.0,
+                "formats": [
+                    "4x4 Mixte"
+                ],
+                "level": [
+                    "Loisir"
+                ]
+            },
+            {
+                "day": "25",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495663200.0,
+                "formats": [
+                    "3x3 Masculin"
+                ],
+                "level": [
+                    "Départemental",
+                    "Régional",
+                    "National",
+                    "Pro"
+                ]
+            },
+            {
+                "day": "25",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495663200.0,
+                "formats": [
+                    "3x3 Féminin"
+                ],
+                "level": [
+                    "Départemental",
+                    "Régional",
+                    "National",
+                    "Pro"
+                ]
+            }
+        ],
+        "href": "http://www.accro-des-tournois.com/fichetournoi.html/2308/massaguel#amobile",
+        "ground": "grass",
+        "new": False,
+        "full": False,
+        "night": False,
+        "city": "Massaguel",
+        "department": "81",
+        "id": 2308,
+        "address": "VILLAGE, 81110 Massaguel, France",
+        "user": "Pascale VAISSIERE",
+        "phone": "06 87 30 80 94",
+        "website": "http://www.cmvb.net",
+        "mail": "http://www.accro-des-tournois.com/contactorga.html/2308/massaguel",
+        "terrains": 15,
+        "inscription": "Prévente : 24€ par équipe\n\nA l'ordre de CMVB",
+        "additional": "Buvette sur place\n1500€ de lots",
+        "publisher": "CMVB"
+    },
+    {
+        "events": [
+            {
+                "day": "27",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495836000.0,
+                "formats": [
+                    "3x3 Masculin"
+                ],
+                "level": [
+                    "National",
+                    "Pro"
+                ]
+            }
+        ],
+        "href": "http://www.accro-des-tournois.com/fichetournoi.html/2376/arles#amobile",
+        "ground": "grass",
+        "new": False,
+        "full": False,
+        "night": False,
+        "city": "Arles",
+        "department": "13",
+        "id": 2376,
+        "name": "3x3 Green \"TOREROS\" masculin",
+        "address": "Stade des cités rue Pierre semard, 13200 Arles, France",
+        "phone": "06.18.74.04.32",
+        "website": "http://https://www.facebook.com/Volley-Ball-Arl%C3%A9sien-954533104677774/",
+        "mail": "http://www.accro-des-tournois.com/contactorga.html/2376/arles",
+        "hour": "09h30",
+        "inscription": "Sur place : 30€ par équipe",
+        "additional": "3x3 masculin sur herbe.\nEn cas de pluie gymnase de repli.\nBuvette et grillade sur place. Soirée et apéro après tournoi.",
+        "publisher": "YohannVBA"
+    },
+    {
+        "events": [
+            {
+                "day": "28",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495922400.0,
+                "formats": [
+                    "4x4 Mixte"
+                ],
+                "level": [
+                    "Départemental",
+                    "Régional",
+                    "National",
+                    "Pro"
+                ]
+            }
+        ],
+        "href": "http://www.accro-des-tournois.com/fichetournoi.html/2315/le-chesnay#amobile",
+        "ground": "indoor",
+        "new": False,
+        "full": True,
+        "night": False,
+        "city": "Le chesnay",
+        "department": "78",
+        "id": 2315,
+        "name": "Tournoi Cellois Chesnay",
+        "address": "Gymnase nouvelle France - gymnase Duchesne, 78150 Le chesnay, France",
+        "user": "Élodie Despierre",
+        "phone": "06 24 44 04 28",
+        "mail": "http://www.accro-des-tournois.com/contactorga.html/2315/le-chesnay",
+        "hour": "8h30",
+        "maxteams": 36,
+        "terrains": 6,
+        "inscription": "Prévente : 40€ par équipe\nSur place : 44€ par équipe\n\nA l'ordre de Cellois Chesnay Volley Hall \nA envoyer au Despierre Élodie 5 rue Pottier 78150 Le chesnay",
+        "additional": "Venez nombreux pour notre 5eme tournoi Cellois-Chesnay.\nUne buvette dans chaque gymnase et de nombreux lots vous attendent.",
+        "publisher": "Elodie"
+    },
+    {
+        "events": [
+            {
+                "day": "28",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495922400.0,
+                "formats": [
+                    "3x3 Masculin"
+                ],
+                "level": [
+                    "Loisir",
+                    "Départemental",
+                    "Régional",
+                    "National",
+                    "Pro",
+                    "Kids"
+                ]
+            },
+            {
+                "day": "28",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495922400.0,
+                "formats": [
+                    "3x3 Féminin"
+                ],
+                "level": [
+                    "Loisir",
+                    "Départemental",
+                    "Régional",
+                    "National",
+                    "Pro",
+                    "Kids"
+                ]
+            }
+        ],
+        "href": "http://www.accro-des-tournois.com/fichetournoi.html/2304/harnes#amobile",
+        "ground": "indoor",
+        "new": False,
+        "full": False,
+        "night": False,
+        "city": "Harnes",
+        "department": "62",
+        "id": 2304,
+        "name": "Squadra Cup Volley 2e édition",
+        "address": "128 chemin Valois, 62440 Harnes, France",
+        "user": "Harnes Volley-Ball",
+        "phone": "0623693535",
+        "website": "http://www.harnes-volleyball.fr",
+        "mail": "http://www.accro-des-tournois.com/contactorga.html/2304/harnes",
+        "maxteams": 50,
+        "terrains": 10,
+        "inscription": "Prévente : 10€ par joueur\nSur place : 15€ par joueur\n\nA l'ordre de Harnes Volley-Ball\nA envoyer au Harnes VB - Squadra Cup Volley - Salle Régionale Maréchal 128 Chemin Valois 62440 Harnes",
+        "additional": "- Inscription via le site internet : www.harnes-volleyball.fr\n- 50 équipes max en Masc / 30 en Fém\n- 1 tee-shirt offert à chaque participant\n- Nombreux lots à gagner !\n- Barbeuc le midi et petite restauration : frites, sandwiches..\n- Parking gratuit\n- 3 Gymnases : Salle Régionale Maréchal, Complexe sportif André Bigotte, Gymnase du Collège Victor Hugo (à proximité de la salle Maréchal)\n- Ouverture : 9H rdv à la salle Maréchal\n- Inscription sur place jusqu'à 9H30 le 28/5\n- Début du tournoi à 9H45/10H\n- A 10 Minutes de Lens, accès routiers/autoroutiers à proximité (A1/A26/A21 N17)",
+        "publisher": "HVB62"
+    },
+    {
+        "events": [
+            {
+                "day": "28",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495922400.0,
+                "formats": [
+                    "3x3 Mixte"
+                ],
+                "level": [
+                    "Loisir",
+                    "Départemental"
+                ]
+            }
+        ],
+        "href": "http://www.accro-des-tournois.com/fichetournoi.html/2389/nogentlerotrou#amobile",
+        "ground": "grass",
+        "new": False,
+        "full": False,
+        "night": False,
+        "city": "Nogent-le-rotrou",
+        "department": "28",
+        "id": 2389,
+        "address": ", 28400 Nogent-le-rotrou, France",
+        "mail": "http://www.accro-des-tournois.com/contactorga.html/2389/nogentlerotrou",
+        "publisher": "Julioguss"
+    },
+    {
+        "events": [
+            {
+                "day": "28",
+                "month": "05",
+                "year": "2017",
+                "timestamp": 1495922400.0,
+                "formats": [
+                    "3x3 Mixte"
+                ],
+                "level": [
+                    "Loisir"
+                ]
+            }
+        ],
+        "href": "http://www.accro-des-tournois.com/fichetournoi.html/2407/damelevieres#amobile",
+        "ground": "grass",
+        "new": True,
+        "full": False,
+        "night": False,
+        "city": "Damelevières",
+        "department": "54",
+        "id": 2407,
+        "name": "Tournoi de la Fête des Mères",
+        "address": "Rue Doct Drouot, 54360 Damelevières, France",
+        "phone": "0660927193",
+        "mail": "http://www.accro-des-tournois.com/contactorga.html/2407/damelevieres",
+        "hour": "9h00",
+        "inscription": "Prévente : 12€ par équipe\nSur place : 14€ par équipe",
+        "publisher": "Romane12"
+    }
+]
+
+@get('/')
+def get_tournaments():
+    return json.dumps(LIST_TOURNAMENTS).encode()
+
+# @post('/')
+# def post_tournaments():
+#     global LIST_TOURNAMENTS
+#     LIST_TOURNAMENTS = parse_tournaments()
+
+
+if __name__ == "__main__":
+    run(host='localhost', port=8080, debug=True)
---
+++ tournament.py
@@ -0,0 +1,183 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+__author__ = "hbuyse"
+
+import re
+import datetime
+import time
+import sqlite3
+import threading
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+# Regex that gives us the ID of the tournament
+PATTERN_ID = re.compile(".*\/(?P<id>\d*)\/.*")
+# Regex that gives us the city and the department of the tournament
+PATTERN_CITY_DEP = re.compile("(?P<city>.*) \((?P<department>[A-Z0-9]{2})\)")
+# Regex that gives us the address of the tournament
+PATTERN_ADDRESS = re.compile("\'(?P<address>.*)\'")
+#Regex that gives us the day, month and year of the tournament
+PATTERN_DATE = re.compile(".* (?P<day>\d{1,2}) (?P<month>.*) (?P<year>\d{4})")
+# Patterns that search for the other critical infos
+PATTERN_HOUR = re.compile("Début du tournoi : ([0-9h]{3,5})")
+PATTERN_GYMNASIUM = re.compile("(\d*) gymnase/stade")
+PATTERN_TERRAINS = re.compile("(\d*) terrains")
+PATTERN_MAXTEAMS = re.compile("(\d*) équipes max.")
+
+
+
+def month_alpha_to_number(month):
+    if month == "Janvier":
+        return str(1)
+    elif month == "Février":
+        return str(2)
+    elif month == "Mars":
+        return str(3)
+    elif month == "Avril":
+        return str(4)
+    elif month == "Mai":
+        return str(5)
+    elif month == "Juin":
+        return str(6)
+    elif month == "Juillet":
+        return str(7)
+    elif month == "Août":
+        return str(8)
+    elif month == "Septembre":
+        return str(9)
+    elif month == "Octobre":
+        return str(10)
+    elif month == "Novembre":
+        return str(11)
+    elif month == "Décembre":
+        return str(12)
+    else:
+        return str(0)
+
+
+class Tournament(object):
+    """
+    Class Tournament
+    """
+    def __init__(self, html_code):
+        self.html_code = html_code
+        self.infos = dict()
+
+    def parse(self):
+        self.infos = {
+            'events': list(),
+
+            # Get its URL and ID
+            'href': self.html_code.find('a').get('href'),
+
+            # Informations on the tournament
+            'ground': self.html_code.find('a').get('class')[0].split('-')[0],
+            'new': bool(re.search("new", self.html_code.find('a').get('class')[0])),
+            'full': bool(re.search("complet", self.html_code.find('a').get('class')[0])),
+            'night': bool(re.search("nuit", self.html_code.find('a').get('class')[0])),
+
+            # Where does it take place?
+            'city': re.search(PATTERN_CITY_DEP, self.html_code.find('h3').string).group("city").strip(),
+            'department': re.search(PATTERN_CITY_DEP, self.html_code.find('h3').string).group("department")
+        }
+
+
+        self.infos['id'] = int(re.search(PATTERN_ID, self.infos['href']).group("id"))
+
+        u = requests.get(self.infos['href'])
+        self.desc = BeautifulSoup(''.join([i.strip().replace('\t', '') for i in u.text.splitlines()]), 'html.parser')
+
+        # Get the name and the address of the tournament
+        try:
+            self.infos['name'] = self.desc.find('div', attrs={"id": "libelletournoi"}).string.strip()
+        except AttributeError as e:
+            pass
+        self.infos['address'] = re.search(PATTERN_ADDRESS, self.desc.find('div', attrs={"id": "tgmap"})['onclick']).group("address")
+
+        # Get the name, the number of the person you have to contact
+        # Get its website address and the address where to send a mail
+        # Get the other informations that can be interesting
+        tdetails = self.desc.find('div', attrs={"id": "tdetails"})
+        for t in tdetails.find_all('p'):
+            if re.search('usericon', str(t)):
+                self.infos['user'] = t.get_text()
+            elif re.search('phoneicon', str(t)):
+                self.infos['phone'] = t.get_text()
+            elif re.search('mouseicon', str(t)):
+                self.infos['website'] = t.a.get('href')
+            elif re.search('mailicon', str(t)):
+                self.infos['mail'] = t.a.get('href')
+            else:
+                other_info = str(t).split('<br/>')
+                for o in other_info:
+                    if re.search(PATTERN_HOUR, o):
+                        self.infos['hour'] = re.search(PATTERN_HOUR, o).group(1)
+                    elif re.search(PATTERN_GYMNASIUM, o):
+                        self.infos['gymnasium'] = int(re.search(PATTERN_GYMNASIUM, o).group(1))
+                    elif re.search(PATTERN_TERRAINS, o):
+                        self.infos['terrains'] = int(re.search(PATTERN_TERRAINS, o).group(1))
+                    elif re.search(PATTERN_MAXTEAMS, o):
+                        self.infos['maxteams'] = int(re.search(PATTERN_MAXTEAMS, o).group(1))
+
+        # # Get the dates of the tournament, its formats and levels
+        tlist = self.desc.find('ul', attrs={"id": "tlist"})
+        for e in tlist.find_all('li', attrs={'class' : "elementtournoi"}):
+            d = dict()
+            r = re.match(PATTERN_DATE, e.h3.get_text().strip())
+            if r:
+                d['day'] = r.group("day").zfill(2)
+                d['month'] = month_alpha_to_number(r.group("month")).zfill(2)
+                d['year'] = r.group("year")
+
+                s = "{}/{}/{}".format(r.group("day").zfill(2), month_alpha_to_number(r.group("month")).zfill(2), r.group("year"))
+                d['timestamp'] = time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())
+
+                d['formats'] =  e.find('div').contents[0],
+                d['level'] = [x.strip() for x in e.find('div').contents[2].split(', ')]
+                self.infos['events'].append(d)
+
+            if re.match("Inscriptions", e.h3.get_text().strip()):
+                self.infos['inscription'] = BeautifulSoup(str(e.div).replace('<br/>', '\n'), 'html.parser').get_text().strip()
+
+            if re.match("En savoir +", e.h3.get_text().strip()):
+                # print("\n".join(e.div.strings))
+                self.infos['additional'] = "\n".join(e.div.strings)
+
+        self.infos['publisher'] = tlist.find('div', attrs={'class' : "align_right"}).a.get_text()
+
+
+    def __str__(self):
+        return "Tournament with ID {} in {} ({})".format(self.infos['id'],self.infos['city'], self.infos['department'])
+
+    def __repr__(self):
+        return "{}({})".format(self.__class__, self.infos['id'])
+
+    def json(self):
+        return self.infos
+
+
+def download_main_page():
+    r = requests.get("http://www.accro-des-tournois.com")
+    content = ''.join([i.strip().replace('\t', '') for i in r.text.splitlines()])
+    return content
+
+
+def parse_tournaments():
+    l = list()
+    s = BeautifulSoup(download_main_page(), 'html.parser')
+
+    elements = s.find_all('li', attrs={"class" : "elementtournoi"})
+    for e in elements:
+        t = Tournament(e)
+        t.parse()
+        print(t)
+        l.append(t.json())
+
+    return l
+
+
+if __name__ == '__main__':
+    print(json.dumps(parse_tournaments(), indent=4, sort_keys=True))
